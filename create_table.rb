47f30fa290f76f2386c20e90ad91b00f61dbabbf
shoma <s.tone@genbasupport.com>
2025-10-21 09:32:03 +0900

    データサイズを指定

--- create_table.rb
+++ create_table.rb
@@ -4,7 +4,7 @@ require 'pg'
 require 'dotenv/load'
 
 PG.connect(dbname: ENV['DB_NAME'], user: ENV['DB_USER'], password: ENV['DB_PASSWORD']) do |conn|
-  conn.exec('CREATE TABLE memos (id SERIAL PRIMARY KEY, title VARCHAR NOT NULL, content TEXT)')
+  conn.exec('CREATE TABLE memos (id SERIAL PRIMARY KEY, title VARCHAR(50) NOT NULL, content TEXT)')
 rescue PG::Error => e
   puts e
 end
