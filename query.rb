c55eb5ad62c879a441e5978528b424d29b90f20b
shoma <s.tone@genbasupport.com>
2025-10-22 14:42:14 +0900

    インデントを修正

--- query.rb
+++ query.rb
@@ -12,19 +12,19 @@ DB_ENV = {
 def select_all
   PG.connect(**DB_ENV) do |conn|
     conn.exec(<<~SQL)
-                SELECT *
-                FROM memos
-                ORDER BY id ASC;
-              SQL
+      SELECT *
+      FROM memos
+      ORDER BY id ASC;
+    SQL
   end
 end
 
 def select(params)
   sql = <<~SQL
-          SELECT *
-          FROM memos
-          WHERE id = $1;
-        SQL
+    SELECT *
+    FROM memos
+    WHERE id = $1;
+  SQL
   bind_values = params.values_at('id')
   PG.connect(**DB_ENV) do |conn|
     conn.exec_params(sql, bind_values)
@@ -33,10 +33,10 @@ end
 
 def store(params)
   sql = <<~SQL
-          INSERT INTO memos
-            (title, content)
-          VALUES ($1, $2);
-        SQL
+    INSERT INTO memos
+      (title, content)
+    VALUES ($1, $2);
+  SQL
   bind_values = params.values_at('title', 'content')
   PG.connect(**DB_ENV) do |conn|
     conn.exec_params(sql, bind_values)
@@ -45,10 +45,10 @@ end
 
 def update(params)
   sql = <<~SQL
-          UPDATE memos
-          SET title = $1, content = $2
-          WHERE id = $3;
-        SQL
+    UPDATE memos
+    SET title = $1, content = $2
+    WHERE id = $3;
+  SQL
   bind_values = params.values_at('title', 'content', 'id')
   PG.connect(**DB_ENV) do |conn|
     conn.exec_params(sql, bind_values)
@@ -57,9 +57,9 @@ end
 
 def destroy(params)
   sql = <<~SQL
-          DELETE FROM memos
-          WHERE id = $1;
-        SQL
+    DELETE FROM memos
+    WHERE id = $1;
+  SQL
   bind_values = params.values_at('id')
   PG.connect(**DB_ENV) do |conn|
     conn.exec_params(sql, bind_values)
