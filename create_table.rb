c1d17c88c412ee0328af967bfd5ad418de76dda2
shoma <s.tone@genbasupport.com>
2025-10-22 16:08:07 +0900

    テーブル作成でもコネクションを再利用するように修正

--- create_table.rb
+++ create_table.rb
@@ -2,13 +2,12 @@
 
 require 'pg'
 require 'dotenv/load'
+require_relative 'query'
 
-PG.connect(dbname: ENV['DB_NAME'], user: ENV['DB_USER'], password: ENV['DB_PASSWORD']) do |conn|
-  conn.exec(<<~SQL)
-    CREATE TABLE memos (
-      id SERIAL PRIMARY KEY,
-      title VARCHAR(50) NOT NULL,
-      content TEXT
-    )
-  SQL
-end
+CONNECTION.exec(<<~SQL)
+  CREATE TABLE memos (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    content TEXT
+  )
+SQL
